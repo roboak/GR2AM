e7f2da914c5cc1d6979d535a726e18246cbfbbe7
Joshua Sonnet <sonnet.joshua@gmail.com>
2021-06-19 01:08:37 +0200

    First batch of training data
    [x] moved old dataset
    [x] added 10 captures each for all 15 gestures
    [ ] had a hard time with index finger up/down 
    [ ] data may have a lot of noise, due to mediapipe TODO check that and add smoothing or similar for huge jumps in coords

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     print(parent_directory)
     parent_directory = Path(parent_directory)
     path = parent_directory / "HandDataset"
-    gestureMetaData = GestureMetaData(gesture_name="gesture1")
+    gestureMetaData = GestureMetaData(gesture_name="gesture_l_down_15")
 
     if platform.system() == "Darwin":  # for me on mac input 1 is the camera
         gesture = GestureCapture(folder_location=str(path), gesture_meta_data=gestureMetaData, camera_input_value=1)
