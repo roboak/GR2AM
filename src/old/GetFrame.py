741def103c967413946f67ead0b84177ae0bdfc6
Abdelrahman Mohamed <abdelrahman.m.selim@gmail.com>
2021-07-14 22:40:37 +0200

    Update GetFrame.py

--- src/old/GetFrame.py
+++ src/old/GetFrame.py
@@ -4,27 +4,56 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import GetHandPoints
-
+from scipy.spatial import distance
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 base_scale = 65
 
+
 def Normalise_Size(hand_data, scale):
     for value in hand_data:
-        temp_dict = {}
-        value.x = value.x*scale
+        value.x = value.x * scale
         value.y = value.y * scale
         value.z = value.z * scale
-    return(hand_data)
+    return hand_data
 
+
+def normalize_scale(image, raw_points):
+    point_5 = return_scaled_hand_cordinates(image, raw_points.multi_hand_landmarks[0].landmark[5])
+    point_17 = return_scaled_hand_cordinates(image, raw_points.multi_hand_landmarks[0].landmark[17])
+    distance_5_17 = distance.euclidean([point_5[0], point_5[1]], [point_17[0], point_17[1]])
+    scale_factor = base_scale / distance_5_17
+    hand_data = raw_points.multi_hand_landmarks[0].landmark
+    hand_data_2 = [{"x": value.x * scale_factor, "y": value.y * scale_factor, "z": value.z * scale_factor}
+                   for value in hand_data]
+    reference_x = hand_data[0].x   # - 0.5  # (image.shape[1] / 2)
+    reference_y = hand_data[0].y   # - 0.5  # (image.shape[0] / 2)
+    for value in hand_data:
+        value.x = value.x * scale_factor
+        value.y = value.y * scale_factor
+        value.z = value.z * scale_factor
+    # TODO: Does it make sense here? I thought to normalize wrt the wrist then translate the whole hand to the middle
+        value.x = value.x - reference_x
+        value.y = value.y - reference_y
+    reference_x = hand_data[0].x - 0.5
+    reference_y = hand_data[0].y - 0.5
+    reference_z = hand_data[0].z - 0.5
+    for value in hand_data:
+        value.x = value.x - reference_x
+        value.y = value.y - reference_y
+        value.z = value.z - reference_z
+
+    return hand_data
 
 
 def plot_landmarks(image, hand_landmarks, color):
     for point in hand_landmarks:
         center_coord = return_scaled_hand_cordinates(image, point)
         cv2.circle(image, center_coord, 10, color, 2)
+
+
 def return_scaled_hand_cordinates(image, point):
-    return (int(point.x*image.shape[1]), int(point.y*image.shape[0]))
+    return int(point.x*image.shape[1]), int(point.y*image.shape[0])
 
 
 def GetFrame():
@@ -42,17 +71,19 @@ def GetFrame():
                 mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
                 mp_drawing.draw_landmarks(plot_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
                 plot_landmarks(plot_image, hand_landmarks.landmark, (255, 255, 255))
-
+            normalized_hand_data = normalize_scale(image, results)
             pt_5 = return_scaled_hand_cordinates(image, results.multi_hand_landmarks[0].landmark[5])
             pt_17 = return_scaled_hand_cordinates(image, results.multi_hand_landmarks[0].landmark[17])
-            distance_5_17 = math.dist([pt_5[0], pt_5[1]], [pt_17[0], pt_17[1]])
-            scale_factor = base_scale/distance_5_17
-            normalised_hand_landmarks_list = Normalise_Size(results.multi_hand_landmarks[0].landmark, scale_factor)
-            print ("Wrist: ", normalised_hand_landmarks_list[0])
-            print ("Middle Finger Top: ", normalised_hand_landmarks_list[12])
-
-            plot_landmarks(plot_image, normalised_hand_landmarks_list, (255, 120, 100))
-            print("length: ", distance_5_17)
+            # distance_5_17 = math.dist([pt_5[0], pt_5[1]], [pt_17[0], pt_17[1]])
+            # distance_5_17 = distance.euclidean([pt_5[0], pt_5[1]], [pt_17[0], pt_17[1]])
+            # scale_factor = base_scale/distance_5_17
+            # normalised_hand_landmarks_list = Normalise_Size(results.multi_hand_landmarks[0].landmark, scale_factor)
+            # pt_5_norm = return_scaled_hand_cordinates(image, normalized_hand_data[5])
+            # pt_17_norm = return_scaled_hand_cordinates(image, normalized_hand_data[17])
+            # normalized_length = distance.euclidean([pt_5_norm[0], pt_5_norm[1]], [pt_17_norm[0], pt_17_norm[1]])
+            plot_landmarks(plot_image, normalized_hand_data, (255, 120, 100))
+            # print("length: ", distance_5_17)
+            # print(f"length normalized: {normalized_length}")
 
         cv2.imshow('MediaPipe Hands', image)
         cv2.imshow('Plot', plot_image)
